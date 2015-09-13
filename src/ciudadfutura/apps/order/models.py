5ec40a65e1f8659c48a0b55376447c11d93740ad
Nicolas Heredia <nicofheredia@gmail.com>
2015-09-13 15:31:32 -0300

    Added new Order app and added a model file

---
+++ src/ciudadfutura/apps/order/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from django.utils import timezone
+
+
+class AutoDateTimeField(models.DateTimeField):
+
+    def pre_save(self, model_instance, add):
+        return timezone.datetime.now()
+
+
+class Order(models.Model):
+
+    reference = models.CharField(max_length=9)
+    status = models.CharField(max_length=32)
+    total = models.DecimalField(max_digits=8, decimal_places=2)
+    invoice = models.CharField(max_digits=255)
+    create_at = models.DateField(default=timezone.now)
+    update_at = models.AutoDateTimeField(default=timezone.now)
+
+
+class OrderItem(models.Model):
+
+    def _get_row_total(self):
+        return self.qty * self.product_price
+
+    order = models.ForeignKey('ciudadfutura_order.Order', related_name='order')
+
+    product = models.ForeignKey('ciudadfutura_product.Product', related_name='product')
+    product_name = models.CharField(max_length=255)
+    product_sku = models.CharField(max_length=255)
+    product_description = models.TextField()
+    product_price = models.DecimalField(max_digits=8, decimal_places=2)
+    product_real_price = models.DecimalField(max_digits=8, decimal_places=2)
+    product_market_price = models.DecimalField(max_digits=8, decimal_places=2)
+    qty = models.IntegerField()
+    row_total = property(_get_row_total())
