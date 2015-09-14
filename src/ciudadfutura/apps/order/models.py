2327d794de7d8438a1e8fd6d395690ebb0527529
Nicolas Heredia <nicofheredia@gmail.com>
2015-09-13 22:58:41 -0300

    Added Invoices and Invoices Item to  Orders

--- src/ciudadfutura/apps/order/models.py
+++ src/ciudadfutura/apps/order/models.py
@@ -11,9 +11,9 @@ class AutoDateTimeField(models.DateTimeField):
 class Order(models.Model):
 
     reference = models.CharField(max_length=9)
-    status = models.CharField(max_length=32)
+    status = models.ForeignKey('ciudadfutura_order.OrderStatus', related_name='status')
     total = models.DecimalField(max_digits=8, decimal_places=2)
-    invoice = models.CharField(max_digits=255)
+    total_items = models.IntegerField()
     user = models.ForeignKey('ciudadfutura_auth.User', related_name='user')
     create_at = models.DateField(default=timezone.now)
     update_at = models.AutoDateTimeField(default=timezone.now)
@@ -26,6 +26,50 @@ class OrderItem(models.Model):
 
     order = models.ForeignKey('ciudadfutura_order.Order', related_name='order')
 
+    product = models.ForeignKey('ciudadfutura_product.Product', related_name='product')
+    product_name = models.CharField(max_length=255)
+    product_sku = models.CharField(max_length=255)
+    product_description = models.TextField()
+    product_price = models.DecimalField(max_digits=8, decimal_places=2)
+    product_real_price = models.DecimalField(max_digits=8, decimal_places=2)
+    product_market_price = models.DecimalField(max_digits=8, decimal_places=2)
+    qty = models.IntegerField()
+    row_total = property(_get_row_total())
+    create_at = models.DateField(default=timezone.now)
+    update_at = models.AutoDateTimeField(default=timezone.now)
+
+
+class OrderStatus(models.Model):
+
+    status = models.CharField(max_length=32)
+    label = models.CharField(max_length=128)
+
+
+class OrderInvoice(models.Model):
+
+    number = models.CharField(max_length=255)
+    total = models.DecimalField(max_digits=8, decimal_places=2)
+    order = models.ForeignKey('ciudadfutura_order.Order', related_name='order')
+    total_itmes = models.IntegerField()
+    user = models.ForeignKey('ciudadfutura_auth.User', related_name='user')
+    user_first_name = models.CharField(max_length=255)
+    user_second_name = models.CharField(max_length=255)
+    user_address = models.CharField(max_length=255)
+    type = models.CharField(max_length=1)
+    note = models.TextField()
+    # transaction = models.ForeignKey('ciudadfutura_transaction.Transaction', related_name='transaction')
+    create_at = models.DateField(default=timezone.now)
+    update_at = models.AutoDateTimeField(default=timezone.now)
+
+
+class OrderInvoiceItem(models.Model):
+
+    @property
+    def _get_row_total(self):
+        return self.qty * self.product_price
+
+    order = models.ForeignKey('ciudadfutura_order.Order', related_name='order')
+    invoice = models.ForeignKey('ciudadfutura_order.OrderInvoice', related_name='invoice')
     product = models.ForeignKey('ciudadfutura_product.Product', related_name='product')
     product_name = models.CharField(max_length=255)
     product_sku = models.CharField(max_length=255)
