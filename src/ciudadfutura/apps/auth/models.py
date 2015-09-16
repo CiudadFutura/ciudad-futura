b6f42538805e2ff3047e557eb1a4be5b76154fe3
Nicolas Heredia <nicofheredia@gmail.com>
2015-09-15 23:39:31 -0300

    Complete person class

--- src/ciudadfutura/apps/auth/models.py
+++ src/ciudadfutura/apps/auth/models.py
@@ -30,15 +30,38 @@ class UserManager(BaseUserManager):
 
 
 class Person(models.Model):
+    first_name = models.CharField(max_length=255)
+    second_name = models.CharField(max_length=255)
+    dni = models.IntegerField()
+    birth_date = models.DateField()
     address = models.CharField(max_length=255)
+    zip_code = models.CharField(max_length=9)
+    city = models.CharField(max_length=255)
+    country = models.CharField(max_length=255)
+    telephone = models.CharField(max_length=32)
+    cellphone = models.CharField(max_length=32)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    tags = models.ManyToManyField('ciudadfutura_auth.Tag')
 
 
 class User(AbstractBaseUser):
+
     name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
-
     is_admin = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
+    reset_password_token = models.CharField()
+    reset_password_sent_at = models.CharField()
+    confirmed_at = models.CharField()
+    confirmation_token = models.CharField()
+    confirmation_sent_at = models.CharField()
+    unconfirmed_email = models.CharField()
+    sign_in_count = models.CharField()
+    current_sign_in_at = models.CharField()
+    last_sign_in_at = models.CharField()
+    current_sign_in_ip = models.CharField()
+    last_sign_in_ip = models.CharField()
 
     USERNAME_FIELD = 'email'
 
@@ -54,6 +77,10 @@ class User(AbstractBaseUser):
 
     def get_short_name(self):
         return self.name or self.email
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
 
 
 class Profile(models.Model):
@@ -78,5 +105,5 @@ class Supplier(Profile):
 
 class MisionMember(Profile):
     user = models.ForeignKey('ciudadfutura_auth.User', related_name='member')
-    circle = models.ForeignKey('ciudadfutura_mision.Circle', null=True, blank=True)
+    circle = models.ForeignKey('ciudadfutura_mision.Circle', related_name='circles')
     is_lead = models.BooleanField(default=False)
