2706fb6383eefda4624f1554020081d7de7b8730
Damian Ignacio Valdés <damian.ignacio@gmail.com>
2015-09-25 16:27:52 -0300

    Moving app mision.

--- src/ciudadfutura/apps/site/urls.py
+++ src/ciudadfutura/apps/site/urls.py
@@ -4,5 +4,18 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^$', views.index, name='ciudadfutura-home'),
+    url(r'^$',
+        views.index,
+        name='ciudadfutura-home'),
+
+    # User urls
+    url(r'^user/login/$',
+        views.user_login,
+        name='ciudadfutura-user-login'),
+    url(r'^user/$',
+        views.index,
+        name='ciudadfutura-user-dashboard'),
+    url(r'^user/profile/$',
+        views.index,
+        name='ciudadfutura-user-profile'),
 ]
--- src/ciudadfutura/apps/site/views.py
+++ src/ciudadfutura/apps/site/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'site/index.html', {})
+
+
+def user_dashboard(request):
+    return render(request, 'site/user_dashboard.html', {})
+
+
+def user_login(request):
+    return render(request, 'site/user_login.html', {})
+
+
+def user_profile(request):
+    return render(request, 'site/user_profile.html', {})
