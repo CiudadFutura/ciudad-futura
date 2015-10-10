e9bcc37a9f3c345a3337af532886e1997a866989
Damian Ignacio Valdés <damian.ignacio@gmail.com>
2015-10-10 15:03:16 -0300

    Register form bugfix.

--- src/ciudadfutura/apps/mision/static/mision/js/register.js
+++ src/ciudadfutura/apps/mision/static/mision/js/register.js
@@ -3,18 +3,39 @@ $(document).ready(function() {
     var holder = $('#invites-holder');
 
     var maxCount = parseInt($('[name="invites"]').attr('max_count'));
+    var minCount = parseInt($('[name="invites"]').attr('min_count'));
 
 
+    var updateIndexes = function () {
+        var total = $('#invites-holder .invite-form').each(function (i, obj) {
+            $(obj).find('.invite-index').html(i + 1);
+        }).length;
+        $('[name="invites"]').val(total);
+    };
+
+    updateIndexes();
+
+    // Add invite
     $('#add-invite').on('click', function (event) {
         event.preventDefault();
         var total = $('#invites-holder .invite-form').length + 1;
         if (total > maxCount) {
             return alert('La canidad de miembros no puede superar: ' + maxCount);
         }
-        var form = tpl.clone();
-        form.find('.invite-index').html(total);
-        holder.append(form);
 
-        $('[name="invites"]').val(total);
+        holder.append(tpl.clone());
+        updateIndexes();
+    });
+
+    // Remove invite
+    $(document).on('click', '#invites-holder .remove', function (event) {
+        event.preventDefault();
+        var total = $('#invites-holder .invite-form').length - 1;
+        if (total < minCount) {
+            return alert('La canidad de miembros no puede ser menor a: ' + minCount);
+        }
+
+        $(this).parents('.invite-form').remove();
+        updateIndexes();
     });
 });
